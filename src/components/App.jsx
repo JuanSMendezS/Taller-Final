fe3c3dad466863ab2143e73bd33195434689e07c
JuanSMendezS <100977862+JuanSMendezS@users.noreply.github.com>
2022-06-05 05:12:39 -0500

    Cambios y ajustes realizados, animaciones agregadas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react'
 import Form from './Form'
 import { firebase } from '../firebase'
 
+
 const App = () => {
+
   const [pokemon, setPokemon] = useState({})
+  const [pokeEdit, setPokeEdit] = useState({})
   const [listaPokemon, setListaPokemon] = useState([])
+  const [modoEdicion, setModoEdicion] = useState(false)
+
 
   const consulta = async () => {
     try {
@@ -15,7 +20,6 @@ const App = () => {
           id: item.id, ...item.data()
         }
       ))
-      console.log(arrayData)
       setListaPokemon(arrayData)
     } catch (err) {
       console.error(err)
@@ -32,14 +36,8 @@ const App = () => {
           img: pokemon.img,
           descripcion: pokemon.descripcion
         }
-        const data = await db.collection('proyecto').add(nuevoReg)
-        const newPokemon = {
-          id: data.id,
-          name: pokemon.name,
-          img: pokemon.img,
-          descripcion: pokemon.descripcion
-        }
-        setListaPokemon([...listaPokemon, newPokemon])
+        await db.collection('proyecto').add(nuevoReg)
+
         setPokemon({})
       } catch (err) {
         console.error(err)
@@ -52,7 +50,7 @@ const App = () => {
 
   React.useEffect(() => {
     consulta()
-  }, [])
+  }, [pokemon])
 
 
   const erase = id => {
@@ -61,55 +59,57 @@ const App = () => {
       db.collection('proyecto').doc(id).delete()
       const aux = listaPokemon.filter(item => item.id !== id)
       setListaPokemon(aux)
+
     } catch (err) {
       console.log(err)
     }
   }
 
-
-  const edit = async (e) => {
-    e.preventDefault()
-    try {
-      const db = firebase.firestore()
-      const data = await db.collection('proyecto')
-      await db.collection('proyecto').doc(data.id).update({
-        id: data.id,
-        name: pokemon.name
-      })
-    } catch (err) {
-      console.error(err)
-    }
+  const editar = item => {
+    setPokeEdit({
+      id: item.id,
+      name: item.name,
+      descripcion: item.descripcion,
+      img: item.img
+    })
+    setModoEdicion(true)
   }
+
 
   return (
     <div className='container'>
       <h1 className='text-center animate__heartBeat'>Aplicación</h1>
       <Form
         setPokemon={setPokemon}
+        modoEdicion={modoEdicion}
+        setModoEdicion={setModoEdicion}
+        pokeEdit={pokeEdit}
+        setListaPokemon={setListaPokemon}
+        listaPokemon={listaPokemon}
       />
       <hr />
       {
         listaPokemon.map((item) => (
-          <div key={item.id} className='d-flex flex-row bd-highlight mb-3 card animate__headShake'>
-            <div className='col-1 align-self-center'>
+          <div key={item.id} className='d-flex flex-row bd-highlight mb-3 card animate__backInLeft'>
+            <div className='col-1 align-self-center animate__backInLeft mx-2'>
               <label htmlFor="Name">{item.name}</label>
             </div>
-            <div className='col-1 align-self-center'>
+            <div className='col-1 align-self-center animate__backInLeft'>
               <label htmlFor="Descripcion">{item.descripcion}</label>
             </div>
-            <div className='col-1 align-self-center animate__headShake'>
-              <img src={item.img} alt={item.id}/>
+            <div className='col-1 align-self-center animate__backInLeft'>
+              <img src={item.img} alt={item.id} />
             </div>
-            <div className='col-1 align-self-center'>
-              <div className='row'>
+            <div className='col-1 align-self-center animate__backInLeft'>
+              <div className='row animate__backInLeft'>
                 <button
-                  className='btn btn-danger btn-block my-1 animate__backInDown'
-                  onClick={erase}>Eliminar</button>
+                  className='btn btn-warning btn-block my-1 animate__backInLeft'
+                  onClick={() => { editar(item) }}>Editar</button>
               </div>
               <div className='row'>
                 <button
-                  className='btn btn-warning btn-block my-1 animate__backInDown'
-                  onClick={edit}>Editar</button>
+                  className='btn btn-danger btn-block my-1 animate__backInLeft' onClick={()=>{erase(item.id)}}
+                >Eliminar</button>
               </div>
             </div>
           </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { Button } from 'react-bootstrap'
-const Form = ({ setPokemon }) => {
+import { firebase } from '../firebase'
+import 'animate.css'
+
+const Form = ({ setPokemon, modoEdicion, setModoEdicion, pokeEdit, listaPokemon, setListaPokemon }) => {
+    const [id, setId] = useState('')
     const [name, setName] = useState('')
     const [descripcion, setDescripcion] = useState('')
+    const [img,setImg]=useState('')
+
     const consultarPokemon = () => {
         axios.get('https://pokeapi.co/api/v2/pokemon/' + name).then((result) => {
             const pokemon = {
                 img: result.data.sprites.front_default,
                 name,
-                descripcion                
+                descripcion
             }
             setPokemon(pokemon)
             setName('')
@@ -18,6 +24,52 @@ const Form = ({ setPokemon }) => {
             console.error(err)
         })
     }
+
+    React.useEffect(() => {
+        if (modoEdicion) {
+            const cargarDatos = () => {
+                setId(pokeEdit.id)
+                setName(pokeEdit.name)
+                setDescripcion(pokeEdit.descripcion)
+                setImg(pokeEdit.img)
+            }
+            cargarDatos()
+        }
+    }, [modoEdicion,pokeEdit])
+
+    const edit = async (e) => {
+        e.preventDefault()
+        try {
+            const db = firebase.firestore()
+            await db.collection('proyecto').doc(id).update({
+                name,
+                descripcion,
+                img
+            })
+            const regEdit = listaPokemon.map(item => item.id === id ? {
+                id,
+                name,
+                descripcion,
+                img
+            } : item)
+
+            setListaPokemon(regEdit)
+            setModoEdicion(false)
+            setId('')
+            setName('')
+            setDescripcion('')
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    const cancelar = (e) => {
+        e.preventDefault()
+        setModoEdicion(false)
+        setId('')
+        setName('')
+        setDescripcion('')
+    }
+
     return (
         <form>
             <label htmlFor="name" className='forn-label'>Nombre del pokemon:</label>
@@ -25,8 +77,14 @@ const Form = ({ setPokemon }) => {
 
             <label htmlFor="name" className='forn-label'>Descripción:</label>
             <input type="text" id='name' className='form-control my-2' value={descripcion} onChange={(e) => { setDescripcion(e.target.value) }} />
-
-            <Button onClick={consultarPokemon}>Buscar Pokemon</Button>
+            {
+                modoEdicion ? (
+                    <>
+                        <Button onClick={(e) => { edit(e) }} className='mx-2'>editar</Button>
+                        <Button onClick={cancelar}>Cancelar</Button>
+                    </>
+                ) : <Button onClick={consultarPokemon}>Buscar Pokemon</Button>
+            }
         </form>
     )
 }
